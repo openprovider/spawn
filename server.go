f808bc19c9ca9b1bf7ee5e6e946bcd04c4aea8bb
Igor Dolzhikov <bluesriverz@gmail.com>
2016-04-23 09:51:06 +0600

    Bumped version number to 0.3.4

--- server.go
+++ server.go
@@ -26,10 +26,10 @@ import (
 const (
 
 	// VERSION - current version of the service
-	VERSION = "0.3.3"
+	VERSION = "0.3.4"
 
 	// DATE - revision date of the service
-	DATE = "2016-04-23T09:09:17Z"
+	DATE = "2016-04-23T09:50:17Z"
 
 	// MaxSignals - maximum count of update signals
 	MaxSignals = 1000
